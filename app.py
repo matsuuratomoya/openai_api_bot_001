8c68a1b04e3cbfaa2845997b528bfb7e4372c6b5
matsuuratomoya <73122103+matsuuratomoya@users.noreply.github.com>
2025-02-22 17:09:49 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content":st.secrets.AppSettings.chatbot_setting"}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4o-mini",
+        messages=messages
+    )
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title("ルサールカ")
+st.image("rusalka.jpg")
+st.write("ルサールカっぽいチャットボットです。")
+
+
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = st.image("user.png")
+        if message["role"]=="assistant":
+            speaker= st.image("rusalka2.jpg")
+
+        st.write(speaker + ": " + message["content"])
